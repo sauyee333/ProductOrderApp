3f45cfae93e1360289d6f01294ddcd614707e42f
sauyee <sauyee333@gmail.com>
2018-01-19 03:56:25 +0800

    update

--- app/src/main/java/tk/ckknight/productqueue/ui/activity/MainActivity.java
+++ app/src/main/java/tk/ckknight/productqueue/ui/activity/MainActivity.java
@@ -3,31 +3,77 @@ package tk.ckknight.productqueue.ui.activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import tk.ckknight.productqueue.R;
 import tk.ckknight.productqueue.ui.fragment.MainFragment;
 import tk.ckknight.productqueue.ui.listener.IFragmentHostListener;
 
 public class MainActivity extends AppCompatActivity implements IFragmentHostListener {
 
+    @BindView(R.id.toolBar)
+    Toolbar mToolBar;
+    @BindView(R.id.appBarLayout)
+    AppBarLayout appBarLayout;
+    @BindView(R.id.topPanel)
+    View topPanel;
+
+    @BindView(R.id.btnInventory)
+    RelativeLayout btnInventory;
+    @BindView(R.id.btnQueueStatus)
+    RelativeLayout btnQueueStatus;
+    @BindView(R.id.btnCart)
+    RelativeLayout btnCart;
+    @BindView(R.id.btnLogin)
+    RelativeLayout btnLogin;
+
     private Context mContext;
     private ProgressDialog mLoadingDialog;
     private Toast mToast;
+
+    @OnClick(R.id.btnInventory)
+    public void loadInventoryPage() {
+        _Debug("loadInventoryPage");
+        Toast.makeText(mContext, "inventory", Toast.LENGTH_LONG);
+    }
+
+    @OnClick(R.id.btnQueueStatus)
+    public void loadQueueStatusPage() {
+        _Debug("loadQueueStatusPage");
+    }
+
+    @OnClick(R.id.btnCart)
+    public void loadCartPage() {
+        _Debug("loadCartPage");
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void loadLoginPage() {
+        _Debug("loadLoginPage");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mContext = MainActivity.this;
-        _Debug("main activity mmmmmmmm");
+        _Debug("2. main activity mmmmmmmm");
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        
         getSupportFragmentManager()
                 .beginTransaction().replace(R.id.content,
                 MainFragment.getInstance()).commit();
